910248cb8750b25f34737da9b253952f18a4ecd2
nagi1999a <nagi1999a.daily@gmail.com>
2021-12-23 22:06:04 +0800

    fix: car rotation (only next)

--- src/controller.js
+++ src/controller.js
@@ -19,11 +19,11 @@ const move = (car, t, isReversed) => {
 
   // rotation
   const tangent = path.getTangentAt(t).normalize();
-  let angle = (tangent.y < 0.1 && tangent.y > -0.1) ? -Math.PI/2 : -Math.atan(tangent.x / tangent.y);
   const up = new THREE.Vector3(0, 0, 1);
-  if (car.onLane == LANE_2 || car.onLane == LANE_3)
-      angle += Math.PI;
-  car.quaternion.setFromAxisAngle(up, angle + Math.PI/2); 
+  let angle = -Math.atan(tangent.x / tangent.y);
+  if (tangent.y < 0)
+    angle += Math.PI;
+  car.quaternion.setFromAxisAngle(up, angle+Math.PI/2); 
   
 };
 export {move};
